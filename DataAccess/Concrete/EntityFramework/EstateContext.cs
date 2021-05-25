da0879f8bbc2a0c88459fd52a6714d3043dbc743
Merve Kibir <68849047+MerveKibir@users.noreply.github.com>
2021-05-25 19:58:50 +0300

    Add files via upload

---
+++ DataAccess/Concrete/EntityFramework/EstateContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EstateContext :DbContext
+    {
+        //Veritabanı tamamlanıp elime ulaştığında bağlantı burada sağlanacak...
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+           
+        //}
+        
+    }
+}
